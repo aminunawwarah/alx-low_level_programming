1b3878d029d048c2a0e7fa613f8836504c3411ba
aminunawwarah <aminunawwarah@gmail.com>
2022-09-08 13:57:02 -0700

    Beginning with Hello C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+*main - prints a string of text to the console
+*
+*Description: using the main function
+*This program prints the text "Programming is like building a
+*multilingual puzzle
+*Return: 0
+*/
 int main(void)
 {
 	char c;
@@ -7,10 +15,11 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
+	
 	printf("Size of a char: %1d byte(s)\n", sizeof(c));
 	printf("Size of an int: %1d byte(s)\n", sizeof(i));
-	printf("Size of a long: %1d byte(s)\n", sizeof(li));
-	printf("Size of a long long: %1d byte(s)\n", sizeof(lli));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %1d byte(s)\n", sizeof(lli));
 	printf("Size of a float: %1d byte(s)\n", sizeof(f));
 	return (0);
 }
