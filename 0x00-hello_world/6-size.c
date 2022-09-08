d6d12b5d37cefcc0617b16bed1f046f7421714c4
aminunawwarah <aminunawwarah@gmail.com>
2022-09-08 14:12:40 -0700

    Beginning with Hello C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-	
+
 	printf("Size of a char: %1d byte(s)\n", sizeof(c));
 	printf("Size of an int: %1d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %1d byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %1d byte(s)\n", sizeof(lli));
 	printf("Size of a float: %1d byte(s)\n", sizeof(f));
-	return(0);
+	return (0);
 }
