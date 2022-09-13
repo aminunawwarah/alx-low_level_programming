4a7bca29cf0e54b4e034da83f3d4ece54618face
aminunawwarah <aminunawwarah@gmail.com>
2022-09-13 12:55:15 -0700

    Functions and Loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,13 @@ void print_alphabet_x10(void)
 {
 	char c;
 	char i;
-	
+
 	for (i = 0; i < 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-		_putchar(c);
+		_putchar('\n');
 	}
 }
