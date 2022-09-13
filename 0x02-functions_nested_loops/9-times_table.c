b03e1c48c2fff6db3dd47a4375a6a52bfb8e0930
aminunawwarah <aminunawwarah@gmail.com>
2022-09-13 13:43:03 -0700

    Functions and Loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 
 	for (number = 0; number <= 9; number++)
 	{
-		_putchar('0');
+		_putchar(' ');
 		for (multiple = 1; multiple <= 9; multiple++)
 		{
 			_putchar(',');
@@ -29,6 +29,7 @@ void times_table(void)
 			{
 				_putchar((product / 10) + '0');
 			}
+			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}
