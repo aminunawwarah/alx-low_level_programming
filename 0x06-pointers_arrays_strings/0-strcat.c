bf5ac9b9ed691e2733d936b78dd95b56ab7ed10f
aminunawwarah <aminunawwarah@gmail.com>
2022-09-22 13:29:45 -0700

    String manupulation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* *_strcat - the function concatenates strings
-*@ is the function parameter
-*@scr is another parameter of the function
+**_strcat - the function concatenates strings
+*@dest: is the function parameter
+*@scr: is another parameter of the function
 *Returns: a pointer
 */
 char *_strcat(char *dest, char *src)
@@ -15,7 +15,7 @@ char *_strcat(char *dest, char *src)
 	{
 		;
 	}
-	for(y = 0; src[y] != '\0'; y++)
+	for (y = 0; src[y] != '\0'; y++)
 	{
 		dest[x] = src[y];
 		x++;
