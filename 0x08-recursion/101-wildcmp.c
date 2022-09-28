fef776873a3d64733b8e460cce34f950ba365d2a
aminunawwarah <aminunawwarah@gmail.com>
2022-09-28 14:06:49 -0700

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -21,7 +21,7 @@ int wildcmp(char *s1, char *2)
 	}
 	if (*s2 == '*')
 	{
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
 	return (0);
 }
