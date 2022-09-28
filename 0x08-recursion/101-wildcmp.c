f0c738cea88a6e5e0a738902f91dc6d0c2a7bacd
aminunawwarah <aminunawwarah@gmail.com>
2022-09-28 14:09:47 -0700

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,7 +5,7 @@
 *@s2: second string
 *Return: 1 if they are the same else 0
 **/
-int wildcmp(char *s1, char *2)
+int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '*' && *(s2 + 1) != '\0' && (s1 == '\0'))
 	{
