f5775ff2922f90225ecea8e58a149122335c167b
aminunawwarah <aminunawwarah@gmail.com>
2022-10-05 13:34:13 -0700

    Memory allocation

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -46,7 +46,7 @@ char **strtow(char *str)
 	{
 		return (NULL);
 	}
-	matrix = (char **) malloc(sizeof(char *) * (words + 1);
+	matrix = (char **) malloc(sizeof(char *)) * (words + 1);
 	if (matrix = NULL)
 	{
 		return (NULL);
