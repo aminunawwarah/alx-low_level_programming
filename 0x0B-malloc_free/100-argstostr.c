5f8220ae71ce6fba9a1e18a05d57aa66e301027c
aminunawwarah <aminunawwarah@gmail.com>
2022-10-05 13:13:11 -0700

    Memory allocation

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*argstostr - program function
+*@ac: is a formal parameter
+*@av: is another formal parameter
+*Return: is the return type of the function
+**/
+char *argstostr(int ac, char **av)
+{
+	int index1, index2, k, size;
+	char *args;
+
+	size = 0;
+	k = 0;
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	index1 = 0;
+	while (index1 < ac)
+	{
+		index2 = 0;
+		while (av[index1][index2])
+		{
+			size++;
+			index2++;
+		}
+		size++;
+		index1++;
+	}
+	arg = malloc((sizeof(char) * size) + 1);
+	if (arg == NULL)
+	{
+		return (NULL);
+	}
+	index1 = 0;
+	while (index1 < ac)
+	{
+		index2 = 0;
+		while (av[index1][index2])
+		{
+			arg[k] = av[index1][index2];
+			index2;
+			k++;
+		}
+		arg[k] = '\n';
+		k++;
+		index1++;
+	}
+	arg[k] = '\0';
+	return (arg);
+}
