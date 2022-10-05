6261fd969fa9974a06925b82aa1d572d2fede74f
aminunawwarah <aminunawwarah@gmail.com>
2022-10-05 13:31:18 -0700

    Memory allocation

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,32 @@
 #include "main.h"
 
 /**
+*wordnos - counts the number of words in a string
+*@s: is the formal parameter of the function
+*Return: the number of words are returned
+**/
+int wordnos(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+		{
+			flag = 0;
+		}
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+	return (w);
+}
+
+/**
 *strtow - the function splits strings
 *@str: is the formal parameter of the function
 *Return: char is the return type of the function
@@ -54,29 +80,4 @@ char **strtow(char *str)
 	}
 	matrix[k] = NULL;
 	return (matrix);
-}
-/**
-*wordnos - counts the number of words in a string
-*@s: is the formal parameter of the function
-*Return: the number of words are returned
-**/
-int wordnos(char *s)
-{
-	int flag, c, w;
-
-	flag = 0;
-	w = 0;
-	for (c = 0; s[c] != '\0'; c++)
-	{
-		if (s[c] == ' ')
-		{
-			flag = 0;
-		}
-		else if (flag == 0)
-		{
-			flag = 1;
-			w++;
-		}
-	}
-	return (w);
 }
