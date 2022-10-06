b261d925c9142f82c8ed0f1d9714e7ee2854b103
aminunawwarah <aminunawwarah@gmail.com>
2022-10-06 13:17:06 -0700

    Memory allocation Part Two

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*array_range - make array of integers
+*@min: least int number
+*@max: max int number
+*Return: returns a pointer of the allocated memory
+**/
+int *array(int min, int max)
+{
+	int index, length;
+	int *a;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	length = max - min + 1;
+	a = malloc(sizeof(int) * length);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (index = 0; index < length; index++, min++)
+	{
+		a[index] = min;
+	}
+	return (a);
+}
