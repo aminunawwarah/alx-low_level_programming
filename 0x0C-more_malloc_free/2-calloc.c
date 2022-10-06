b2f174daa103536efa3e2c9a1b69099f34720016
aminunawwarah <aminunawwarah@gmail.com>
2022-10-06 13:32:23 -0700

    Memory allocation Part Two

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (index = 0; index < (nmemb * size) index++)
+	for (index = 0; index < (nmemb * size); index++)
 	{
 		p[index] = 0;
 	}
