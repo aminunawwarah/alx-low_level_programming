14924a8acb81e66b88664c726c781626ed8d181d
aminunawwarah <aminunawwarah@gmail.com>
2022-10-06 13:21:21 -0700

    Memory allocation Part Two

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,44 +14,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 
 	if (s1 == NULL)
-	{
 		index1 = 0;
-	}
 	else
 	{
 		for (index1 = 0; s1[index1]; index1++)
-		{
 			;
-		}
 	}
 	if (s2 == NULL)
-		{
 			index2 = 0;
-		}
 		else
 		{
 			for (index2 = 0; s2[index2]; index2++)
-			{
 				;
-			}
 		}
 		if (index2 > n)
-		{
 			index2 = n;
-		}
 		s = malloc(sizeof(char) * (index1 + index2 + 1));
 		if (s == NULL)
-		{
 			return (NULL);
-		}
 		for (k = 0; k < index1; k++)
-		{
 			s[k] = s1[k];
-		}
 		for (k = 0; k < index2; k++)
-		{
 			s[k + index1] = s2[k];
-		}
 		s[index1 + index2] = '\0';
 		return (s);
 }
