e07bfb7c3da83eb3a3f62561921ba2b2b0f465c4
aminunawwarah <aminunawwarah@gmail.com>
2022-10-11 03:36:49 -0700

    Structures and typedef source files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef STRTYP
+#define STRTYP
 /**
-*struct dog -structure of a dog
+*struct dog - structure of a dog
 *@name: name of dog
 *@owner: owner of dog
 *@age: age of dot
@@ -13,10 +13,12 @@ float age;
 char *owner;
 
 };
+
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog (char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*STRTYP*/
+#endif
