7f5f38bbbca5f7e7f54d47f140f9363ff63a7298
aminunawwarah <aminunawwarah@gmail.com>
2022-12-08 13:20:22 -0800

    Doubly-linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,4 +11,39 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *newnode;
+	dlistint_t *traverse;
+	unsigned int pos;
+
+	if (h == NULL)
+		return (NULL);
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	if (idx == 0)
+	{
+		newnode->next = *h;
+		newnode->prev = NULL;
+		if (newnode->next != NULL)
+			newnode->next->prev = newnode;
+		*h = newnode;
+	}
+	else
+	{
+		traverse = *h;
+		for (pos = 0; traverse != NULL && pos < (idx - 1); pos++)
+			traverse = traverse->next;
+		if (traverse == NULL)
+		{
+			free(newnode);
+			return (NULL);
+		}
+		newnode->next = traverse->next;
+		newnode->prev = traverse;
+		if (traverse->next != NULL)
+			traverse->next->prev = newnode;
+		traverse->next = newnode;
+	}
+	return (newnode);
 }
